4d42242e8ac0479be22f483b3457c4308f607ec2
piotr <pskirski@hotmail.com>
2014-08-19 22:15:00 +0200

    Added landing page

--- Rachunki/App_Start/BundleConfig.cs
+++ Rachunki/App_Start/BundleConfig.cs
@@ -18,7 +18,11 @@ namespace Rachunki
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js"
+                      ));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "~/Scripts/angular.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
